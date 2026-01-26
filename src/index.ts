7c76083f4237128d148e28212eb26a120343edbc
Zwivhuya <zwivhuya805@gmail.com>
2026-01-26 14:14:19 +0200

    feat(connection pool): add connection pool

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import app from "./app";
+import pool from "./config/db"; 
 
 const PORT = process.env.PORT || 3000;
+const testConnection = async () => {
+  try {
+    await pool.query('SELECT NOW()');
+    console.log('Database connection successful');
+  } catch (error) {
+    console.error('Database connection failed:', error);
+    process.exit(1);
+  }
+};
 
+testConnection();
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
 });

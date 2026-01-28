f75c227a1ac043e8bbe1a7f1fd92357c2d6163cc
Karabo2002dev <kgapholakarabo01@gmail.com>
2026-01-28 11:15:38 +0200

    feat(middleware-implementation): Implement middleware on routes

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,57 +1,46 @@
-import { Request, Response } from "express";
-import * as authService from "../services/authService";
+import { Request, Response, NextFunction } from 'express';
+import * as authService from '../services/authService';
 
-
-export const register = async (req: Request, res: Response): Promise<void> => {
+export const register = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
   try {
     const { email, password, role, fullName, phoneNumber } = req.body;
 
-    if (!email || !password) {
-      res.status(400).json({ message: "Email and password are required" });
-      return;
-    }
-
-    if(!fullName) {
-        res.status(400).json({ message: "Full name is required" });
-        return;
-    }
-
-    if(!phoneNumber) {
-        res.status(400).json({ message: "Phone number is required" });
-        return;
-    }
-
-    const user = await authService.register(email, password, phoneNumber, fullName, role);
+    const user = await authService.register(
+      email,
+      password,
+      phoneNumber,
+      fullName,
+      role
+    );
 
     res.status(201).json({
-      message: "User registered successfully",
+      message: 'User registered successfully',
       user,
     });
-  } catch (err: any) {
-    res.status(400).json({
-      message: err.message || "Registration failed",
-    });
+  } catch (err) {
+    next(err); // 🔥 send to global error handler
   }
 };
 
-export const login = async (req: Request, res: Response): Promise<void> => {
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
   try {
     const { token } = req.body;
-
-    if (!token) {
-      res.status(400).json({ message: "Token is required" });
-      return;
-    }
 
     const user = await authService.login(token);
 
     res.status(200).json({
-      message: "Login successful",
+      message: 'Login successful',
       user,
     });
-  } catch (err: any) {
-    res.status(401).json({
-      message: err.message || "Login failed",
-    });
+  } catch (err) {
+    next(err);
   }
 };

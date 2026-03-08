19c8569286adc1b775bfc52e4c4bf2d9a6102b2e
Khaphathe Mpho <khaphathempho@gmail.com>
2026-03-08 23:04:12 +0200

    Added a setting crud method

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,10 +2,18 @@ import { Role } from "../types/role";
 import { Router } from "express";
 import { verifyFirebaseToken } from "../middlewares/authMiddleware";
 import { roleAuth } from "../middlewares/roleMiddleware";
-import { fetchAllUsers } from "../controllers/userController";
+import {
+  fetchAllUsers,
+  editProfile,
+  removeProfile,
+} from "../controllers/userController";
 
 const router = Router();
 
 router.get("/", verifyFirebaseToken, roleAuth([Role.Admin]), fetchAllUsers);
+
+router.get("/users", fetchAllUsers);
+router.put("/profile/:id", editProfile);
+router.delete("/profile/:id", removeProfile);
 
 export default router;
